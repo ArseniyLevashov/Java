f7736556017140feb1995af8050b3fe638993642
ArseniyLevashov <59114768+ArseniyLevashov@users.noreply.github.com>
2021-07-01 18:16:48 +0300

    Add files via upload

---
+++ spr/src/main/java/main.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class main {
+
+    @SpringBootApplication
+    public static class Application {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+
+        }
+
+    }
+}
